dd6b65266ed8123aeff1a5b3b5690acc7b4b49b8
nothing0 <jopiejopie8@gmail.com>
2013-09-18 14:20:10 +0200

    Update config.php

--- config.php
+++ config.php
@@ -4,20 +4,20 @@
  * Twitteroauth keys
  * ===================================
  */
-define('CONSUMER_KEY', 			'qh9hBmi4LvU8l7alrGA');
-define('CONSUMER_SECRET', 		'86cLVpBGMxg3cJBi1pYtFNRCIhDJ1uh4NctVMWAfPs');
-define('OAUTH_TOKEN', 			'285133869-BKSgxOoBHdPY0ksgijWP5JMbJ3xW2Xq9JJqxkTKm');
-define('OAUTH_TOKEN_SECRET',	'R1R7SRz9ZIA75B8lmaPHyHWzqwSPDAfMP17QFQtNE');
+define('CONSUMER_KEY', 			'');
+define('CONSUMER_SECRET', 		'');
+define('OAUTH_TOKEN', 			'');
+define('OAUTH_TOKEN_SECRET',	'');
 
 /* ===================================
  * Global
  * ===================================
  */
 // ROOT_PATH is the path to this location where all the twitter-command files are stored
-define('ROOT_PATH',				'/Applications/xampp/htdocs/twitter-command/');
+define('ROOT_PATH',				'/path/to/project/twitter-command/');
 
 // PROJECT_PATH is the path to the project where twitter-command is used for, so that some files don't have to be in the ROOT_PATH
-define('PROJECT_PATH',			'/Applications/xampp/htdocs/twitter-command/');
+define('PROJECT_PATH',			'/path/to/project/twitter-command/');
 
 // The name of the actions file
 define('ACTIONS_FILE', 			ROOT_PATH . 'actions.php');
